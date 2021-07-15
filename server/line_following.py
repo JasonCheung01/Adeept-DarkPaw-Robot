ee1e49ce051eacbaa32a5f3742e70af6771761e3
Jason Cheung <jcheung1@pride.hofstra.edu>
2021-07-15 11:49:18 -0900

    created a text display that reports what line color robot is detecting, got basic principle of line tracking algorithm (analyzes two pixel in parallel (orange is left pixel and blue is right pixel))

--- server/line_following.py
+++ server/line_following.py
@@ -1,12 +1,3 @@
-# Psuedocode: 
-# 1) Start the Camera (done) 
-# 2) Filter an image by color (done) 
-# 3) Pick a threshold for the image to have a binary image (done) 
-# 3a) Reason why we go from RGB to binary is b/c it allows us to select pixels of interest according to where they lie in colour space (explanation)  
-# 4) Detect the line (done) 
-# 5) Robot moves while simultaneously trying to keep track of the line 
-# 6) Robot stops when it reaches the end of the line
-
 import os
 import cv2 
 import time 
@@ -21,10 +12,10 @@ from camera_opencv import Camera
 CVRun = 1
 linePos_1 = 440
 linePos_2 = 380
-lineColorSet = 0		# 255 for white line and 0 for black line 
+lineColorSet = 0	# 255 for white line and 0 for black line 
 frameRender = 1
-findLineError = 20		# How far off it is from the center coordinate when tracking for a line		
-setCenter = 320			# Set center coordinate when tracking for a line 
+findLineError = 20	# How far off it is from the center coordinate when tracking for a line		
+setCenter = 320		# Set center coordinate when tracking for a line 
 
 colorUpper = np.array([44, 255, 255])
 colorLower = np.array([24, 100, 100])
@@ -62,7 +53,7 @@ while(True):
 
 	# Line detection algorithm
 	try:  
-		# sums of array elements over a given axis (for a white line)
+		# sums of array elements over a given axis 
 		lineColorCount_Pos1 = np.sum(colorPos_1 == lineColorSet) 
 		lineColorCount_Pos2 = np.sum(colorPos_2 == lineColorSet)
 
@@ -84,32 +75,37 @@ while(True):
 		center_Pos2 = int((left_Pos2 + right_Pos2) / 2)
 
 		center = int((center_Pos1 + center_Pos2) / 2)   
+
+		if lineColorSet == 255:  
+			cv2.putText(frame,('Following White Line'),(30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128),1,cv2.LINE_AA) 
+		elif lineColorSet == 0: 
+			cv2.putText(frame,('Following Black Line'),(30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128),1,cv2.LINE_AA)		 
+		else: 
+			cv2.putText(frame,('No Line Detected'),(30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128),1,cv2.LINE_AA)
 	
-		# Code below tracks the line 
+		# Code below tracks the line by analyzing two pixels in parallel and utilizes the information it detects 
+		# x coordinate of starting and ending are always changing 
 
-		# Syntax: cv2.line(image, start_point, end_point, color, thickness)
-		# Forms a white cross that goes through bottom part of the retangular box	
-		cv2.line(frame,(left_Pos1,(linePos_1+30)),(left_Pos1,(linePos_1-30)),(255,128,64),1)
-		cv2.line(frame,(right_Pos1,(linePos_1+30)),(right_Pos1,(linePos_1-30)),(64,128,255),)
-		cv2.line(frame,(0,linePos_1),(640,linePos_1),(255,255,64),1)
+		# Forms a cross that goes through bottom part of the retangular box	
+		cv2.line(frame,(left_Pos1,(linePos_1+30)),(left_Pos1,(linePos_1-30)),(255,128,64),1)	# vertical line of cross (orange)
+		cv2.line(frame,(right_Pos1,(linePos_1+30)),(right_Pos1,(linePos_1-30)),(64,128,255),) # vertical line of cross (blue)
+		#cv2.line(frame,(0,linePos_1),(640,linePos_1),(255,255,64),1)	# horizontal line of rectangular box (turquoise)
 
-		# Forms a white cross that goes through the top part of the rectangular box
-		cv2.line(frame,(left_Pos2,(linePos_2+30)),(left_Pos2,(linePos_2-30)),(255,128,64),1)
-		cv2.line(frame,(right_Pos2,(linePos_2+30)),(right_Pos2,(linePos_2-30)),(64,128,255),1)
-		cv2.line(frame,(0,linePos_2),(640,linePos_2),(255,255,64),1)
+		# Forms a cross that goes through the top part of the rectangular box
+		#cv2.line(frame,(left_Pos2,(linePos_2+30)),(left_Pos2,(linePos_2-30)),(255,128,64),1)
+		#cv2.line(frame,(right_Pos2,(linePos_2+30)),(right_Pos2,(linePos_2-30)),(64,128,255),1)
+		#cv2.line(frame,(0,linePos_2),(640,linePos_2),(255,255,64),1)
 		
 		# Forms a black cross that goes through the middle of the rectangular box 
-		cv2.line(frame,((center-20),int((linePos_1+linePos_2)/2)),((center+20),int((linePos_1+linePos_2)/2)),(0,0,0),1)
-		cv2.line(frame,((center),int((linePos_1+linePos_2)/2+20)),((center),int((linePos_1+linePos_2)/2-20)),(0,0,0),1) 
+		#cv2.line(frame,((center-20),int((linePos_1+linePos_2)/2)),((center+20),int((linePos_1+linePos_2)/2)),(0,0,0),1)
+		#cv2.line(frame,((center),int((linePos_1+linePos_2)/2+20)),((center),int((linePos_1+linePos_2)/2-20)),(0,0,0),1) 
 		
-		if center > (setCenter + findLineError):  
-			SpiderG.walk('turnright')  
-
-		elif center < (setCenter - findLineError): 	 
-			SpiderG.walk('turnleft') 
-		
-		else: 
-			SpiderG.walk('forward')  
+		#if center > (setCenter + findLineError):  
+			#SpiderG.walk('turnright')  
+		#elif center < (setCenter - findLineError): 	 
+			#SpiderG.walk('turnleft') 
+		#else: 
+			#SpiderG.walk('forward')  
 	
 	# if an exception occur during the try clause, go here
 	except:   
