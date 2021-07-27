294610581d19dbaf0c307f1d3d6b72ee0a51b451
Jason Cheung <jcheung1@pride.hofstra.edu>
2021-07-27 09:20:11 -0900

    changed method to how we drew intersection point of line and contour, added a try and except to prevent code from breaking randomly when testing robot

--- server/line_following.py
+++ server/line_following.py
@@ -28,37 +28,83 @@ while(True):
 	frame_blur = cv2.GaussianBlur(frame_gray,(5,5),0) 
 
 	# Color Thresholding 
-	ret, thresh = cv2.threshold(frame_blur, 60, 255, cv2.THRESH_BINARY_INV) 
+	ret, thresh = cv2.threshold(frame_blur, 60, 255, cv2.THRESH_BINARY_INV)  
 
-	# Assuming there is only one contour conts[0] 
-	contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE) 
-	x, y, w, h = cv2.boundingRect(contours[0])  
+	try:
+		# Assuming there is only one contour conts[0] 
+		contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE) 
+		x, y, w, h = cv2.boundingRect(contours[0])  
 
-	# Draws a rectangle around the first contour of the frame 
-	frame = cv2.cvtColor(frame_gray, cv2.COLOR_GRAY2BGR) 
-	cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)  
-	cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
+		# Draws a rectangle around the first contour of the frame 
+		frame = cv2.cvtColor(frame_gray, cv2.COLOR_GRAY2BGR) 
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)  
+		cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
 
-	# Draws our two line on the rectangle 
-	cv2.line(frame, (x, y + h // 3), (x + w, y + h // 3), (255,0,0), 1)  
-	cv2.line(frame, (x, y + h // 3 * 2), (x + w, y+ h // 3 * 2), (255,0,0), 1)	
+		# Draws our two line on the rectangle 
+		cv2.line(frame, (x, y + h // 3), (x + w, y + h // 3), (255,0,0), 1)  
+		cv2.line(frame, (x, y + h // 3 * 2), (x + w, y + h // 3 * 2), (255,0,0), 1)	
  
-	points = []  
-	y1 = y + h // 3 
-	y2 = y + h // 3 * 2   
-	contour = contours[0] 
+		#points = []  
+		y1 = y + h // 3 
+		y2 = y + h // 3 * 2   
+		contour = contours[0] 
 
-	# appends the intersection point between lines and contours to a list  
-	for y in (y1,y2):  
-		points.append(contour[contour[:,:,1] == y])  
-		test = contour[contour[:,:,1] == y]  
-	
-	# draws the intersection point between lines and contours from the list 
-	for x in points:   
-		# coordinates = tuple(x)
-		cv2.circle(frame, (x[0][0],x[0][1]), 3, (0,0,255), 3) 
-		cv2.circle(frame, (x[1][0],x[1][1]), 3, (0,0,255), 3)
-				
+		#----------------------------------------------------------------#
+		# Initial Method for interestction of line and contour: 
+		#----------------------------------------------------------------#
+
+		# appends the intersection point between lines and contours to a list  
+		# for y in (y1,y2):  
+			#points.append(contour[contour[:,:,1] == y])  
+			#test = contour[contour[:,:,1] == y]  
+			#print(test) 
+
+		#y1_x1 = points[0][0][0]
+                #y1_x2 = points[0][1][0]
+                #y2_x1 = points[1][0][0]
+                #y2_x2 = points[1][1][0]
+
+                #x1 = (y1_x1 + y1_x2)/2
+                #x2 = (y2_x1 + y2_x2)/2
+
+                #m = (y2-y1)/(x2-x1)
+
+                #cv2.line(img, (x1,y1), (x2,y2), (255, 255, 0), 1)
+
+                #draws the intersection point between lines and contours from the list
+                #for x in points:   
+                        #coordinates = tuple(x) 
+                        #print(x[0][0])
+                        #print("hello") 
+                        #print(x[0][1]) 
+                        #print(x)
+                        #print("-----------")
+                        #cv2.circle(frame, (x[0][0], x[0][1]), 3, (0,0,255), 3) # draws the two circle on the left
+                        #cv2.circle(frame, (x[1][0], x[1][1]), 3, (0,0,255), 3) # draws the two circle on the right
+                        #cv2.line(frame, (x[0][0], x[1][1]),(x[0][1], x[1][0]), (0,0,255), 2)
+                        #cv2.line(frame, (x[1][0]), (x[1][1]), (0,0,255), 2)
+ 
+		#----------------------------------------------------------------#  
+
+		# Draws the intersection point between our first horizontal line and the contour
+		y1_points = contour[contour[:,:,1] == y1] 
+		cv2.circle(frame, (y1_points[0][0], y1_points[0][1]), 3, (0,0,255), 3) 
+		cv2.circle(frame, (y1_points[1][0], y1_points[1][1]), 3, (0,0,255), 3) 
+
+		# Draws the intersection point between our second horizontal line and the contour
+		y2_points = contour[contour[:,:,1] == y2] 
+		cv2.circle(frame, (y2_points[0][0], y2_points[0][1]), 3, (0,0,255), 3) 
+		cv2.circle(frame, (y2_points[1][0], y2_points[1][1]), 3, (0,0,255), 3) 
+
+		# Draws our slope connecting the intersection points
+		cv2.line(frame, (y1_points[0][0], y1_points[0][1]),(y2_points[0][0], y2_points[0][1]), (0,0,255), 2)
+		cv2.line(frame, (y1_points[1][0], y1_points[1][1]),(y2_points[1][0], y2_points[1][1]), (0,0,255), 2)	 
+
+		# Calculate the slope connecting the interesction points 
+			
+
+	except:	  
+		pass 		
  
 	# Display the resulting frame on screen 
 	cv2.imshow('display', frame)  
