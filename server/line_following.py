0bc93f18a7dedfa067eaa20aee66d9666bdc9664
Jason Cheung <jcheung1@pride.hofstra.edu>
2021-08-02 11:18:26 -0900

    reimplemented robot movement in account to keeping line near center of window

--- server/line_following.py
+++ server/line_following.py
@@ -24,6 +24,9 @@ while(True):
 	# Capture the video frame by frame
 	ret, frame = vid.read()   
 	
+	# Resize of frame window
+	#frame = cv2.resize(frame, (700,540)) 
+	
 	# Adjust brightness of video frame	
 	cols, rows = frame.shape[-2:] 
 	brightness = np.sum(frame) / (255 * cols * rows)
@@ -40,7 +43,7 @@ while(True):
 	frame_blur = cv2.GaussianBlur(frame_gray,(3,3),0) 
 
 	# Color Thresholding 
-	ret, thresh = cv2.threshold(frame_blur, 50, 255, cv2.THRESH_BINARY_INV)  
+	ret, thresh = cv2.threshold(frame_blur, 50, 255, cv2.THRESH_BINARY_INV)   
 	
 	try:  
 		# Assuming there is only one contour conts[0]
@@ -71,7 +74,7 @@ while(True):
 			# Calculate and draw the midpoint of the intersection point between our first horizontal line and the contour 
 			y1_x_mp = (y1_points[0][0] + y1_points[1][0]) // 2 
 			y1_y_mp = (y1_points[0][1] + y1_points[1][1]) // 2 
-			#cv2.circle(frame, (y1_x_mp, y1_y_mp), 3, (127,0,63), 3)  
+			cv2.circle(frame, (y1_x_mp, y1_y_mp), 3, (127,0,63), 3)  
 
 			# Draws the intersection point between our second horizontal line and the contour
 			y2_points = contour[contour[:,:,1] == y2] 
@@ -81,7 +84,7 @@ while(True):
 			# Calculate and draw the midpoint of the intersection point between our second horizontal line and the contour 
 			y2_x_mp = (y2_points[0][0] + y2_points[1][0]) // 2 
 			y2_y_mp = (y2_points[0][1] + y2_points[1][1]) // 2 
-			#cv2.circle(frame, (y2_x_mp, y2_y_mp), 3, (127,0,63), 3) 
+			cv2.circle(frame, (y2_x_mp, y2_y_mp), 3, (127,0,63), 3) 
 
 			# Calculate and draw the slope of the midpoint  
 			m = (y2_y_mp - y1_y_mp) / (y2_x_mp - y1_x_mp) 
@@ -89,7 +92,45 @@ while(True):
 
 			# Display slope of the midpoint on screen 
 			cv2.putText(frame,('Slope: ' + str(m)), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)   
-		    
+
+                        # Calculate and draw the center x and y coordinate of the two midpoint 
+			center_mp_y = (y1_y_mp + y2_y_mp) // 2  
+			center_mp_x = (y1_x_mp + y2_x_mp) // 2 
+			cv2.circle(frame, (center_mp_x, center_mp_y), 3, (252,186,3), 3)   
+
+			# Calculate how far off the center y coordinate is from center of OpenCV window frame (Xm - 1/2 * width) 
+			alignment_error = center_mp_x - (0.5 * frame.shape[1])    
+
+			# Display alignment error on screen  
+			cv2.putText(frame,('Alignment Error: ' + str(alignment_error)), (30,80), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA) 
+
+			# alignment is off and slope is off 
+			if (abs(m) < 3 and abs(alignment_error) > 12): 
+				if (m > 0):  
+					# m is + && a is + (slope to left and line on right)
+					if (alignment_error > 0): 
+						cv2.putText(frame,('Walking Forward 1'), (30,110), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)
+						#SpiderG.walk('forward') 
+					
+					# m is + && a is - (slope to left and line on left)
+					else:  
+						cv2.putText(frame,('Turning Left'), (30,110), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)
+						#SpiderG.walk('turnleft') 
+
+				else: 
+					cv2.putText(frame,('Turning Right 1'), (30,110), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)
+                                        #SpiderG.walk('turnright')
+			else:   
+				# m is - && a is + (slope to right and line on right)
+				if (alignment_error > 0):  
+					cv2.putText(frame,('Turning Right 2'), (30,110), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)
+					#SpiderG.walk('turnright')  
+					
+				# m is - && a is - (slope to right and line on left)
+				else:  
+					cv2.putText(frame,('Walking Forward 2'), (30,110), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(128,255,128), 1, cv2.LINE_AA)
+					#SpiderG.walk('forward')  
+					
 			#if (abs(m) < 2):  
 				# Add slope into our queue 
 				#queue.append(m)
